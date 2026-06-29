c6c9f4d51d0a49e41c3b515d5f0c3d340c072259
Milan Nikolic <gen2brain@gmail.com>
2026-06-29 19:06:08 +0200

    Try several lib names in Windows

--- purego_windows.go
+++ purego_windows.go
@@ -8,16 +8,19 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-const (
-	libname = "libmpv.dll"
-)
+// libnames are the libmpv DLL names used by various distributions, tried in order.
+var libnames = []string{"libmpv-2.dll", "libmpv.dll", "mpv-2.dll", "mpv-1.dll"}
 
-// loadLibrary loads the dll and panics on error.
+// loadLibrary loads the dll and panics if none of the known names can be found.
 func loadLibrary() uintptr {
-	handle, err := windows.LoadLibrary(libname)
-	if err != nil {
-		panic(fmt.Errorf("cannot load library %s: %w", libname, err))
+	var err error
+	for _, name := range libnames {
+		var handle windows.Handle
+		handle, err = windows.LoadLibrary(name)
+		if err == nil {
+			return uintptr(handle)
+		}
 	}
 
-	return uintptr(handle)
+	panic(fmt.Errorf("cannot load libmpv (tried %v): %w", libnames, err))
 }
